00d9765602d237fd8323a76a98b0de86be32aee9
Jessica <jessicagerente@gmail.com>
2022-02-05 11:03:52 -0300

    add dataframe analysis

--- scraping_examples.py
+++ scraping_examples.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 # examples from:
@@ -38,3 +39,27 @@ seven_day = soup.find(id="seven-day-forecast")
 forecast_items = seven_day.find_all(class_="tombstone-container")
 tonight = forecast_items[0]
 print(tonight.prettify())
+
+period = tonight.find(class_ = "period-name").get_text()
+short_desc = tonight.find(class_ = "short-desc").get_text()
+temp = tonight.find(class_ = "temp").get_text()
+img = tonight.find("img")
+desc = img['title']
+
+period_tags = seven_day.select(".tombstone-container .period-name")
+periods = [pt.get_text() for pt in period_tags]
+
+short_descs = [sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
+temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
+descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
+
+weather = pd.DataFrame({
+    "period": periods,
+    "short_desc": short_descs,
+    "temp": temps,
+    "desc":descs
+})
+
+temp_nums = weather["temp"].str.extract("(?Pd+)", expand=False)
+
+weather["temp"].str.extract(r'([ab])(\d)')
