863332eb5f38c5fb34f7d83a0bbfb7a75f252198
Jessica <jessicagerente@gmail.com>
2022-02-06 17:19:45 -0300

    add fixes

--- flask_example/basic_example.py
+++ flask_example/basic_example.py
@@ -1,7 +1,7 @@
 __author__ = 'jessica'
-
+from distutils.log import debug
+import json
 import requests
-import folium
 import pandas as pd
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
@@ -12,18 +12,11 @@ app = Flask(__name__)
 def index():
     return render_template('test.html')
 
-@app.route('/map')
-def map():
-    start_coords = (46.9540700, 142.7360300)
-    folium_map = folium.Map(location=start_coords, zoom_start=14)
-    folium_map.add_child(folium.LatLngPopup())
-    return folium_map._repr_html_()
-
 @app.route('/forecast', methods=['POST'])
 def forecast():
     lat = request.form['lat']
     long = request.form['long']
-
+    source = f"https://forecast.weather.gov/MapClick.php?lat={lat}&lon={long}"
     page = requests.get(f"https://forecast.weather.gov/MapClick.php?lat={lat}&lon={long}")
     soup = BeautifulSoup(page.content, 'html.parser')
     seven_day = soup.find(id="seven-day-forecast")
@@ -39,10 +32,18 @@ def forecast():
         "Long description": descs
     })
 
-    return 'Hi, your summarized forecast is: %s <br/> <a href="/">Back Home</a>' % (weather.to_html(header="true", table_id="table")
-)
+    return ('<h3> Your summarized forecast is: </h3> %s <br/> <a href="/">Back Home</a>'
+    % (weather.to_html(header="true", table_id="table")))
 
+@app.route('/test', methods=['POST'])
+def test():
+    output = request.get_json()
+    print(output) # This is the output that was stored in the JSON within the browser
+    print(type(output))
+    result = json.loads(output) #this converts the json output to a python dictionary
+    print(result) # Printing the new dictionary
+    print(type(result))#this shows the json converted as a python dictionary
+    return result
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host = '0.0.0.0', port = 3009)
+    app.run(host = '0.0.0.0', port = 3021, debug = True)
--- scraping_examples.py
+++ scraping_examples.py
@@ -36,6 +36,7 @@ soup.select("div p")
 page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
 soup = BeautifulSoup(page.content, 'html.parser')
 seven_day = soup.find(id="seven-day-forecast")
+
 forecast_items = seven_day.find_all(class_="tombstone-container")
 tonight = forecast_items[0]
 print(tonight.prettify())
@@ -69,3 +70,19 @@ is_night = weather["temp"].str.contains("Low")
 weather["is_night"] = is_night
 
 weather[is_night]
+
+
+page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+soup = BeautifulSoup(page.content, 'html.parser')
+seven_day = soup.find(id="seven-day-forecast")
+periods = [pt.get_text() for pt in seven_day.select(".tombstone-container .period-name")]
+short_descs = [sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
+temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
+descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
+
+weather = pd.DataFrame({
+    "period": periods,
+    "short_desc": short_descs,
+    "temp": temps,
+    "desc": descs
+})
